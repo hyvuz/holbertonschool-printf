5a0c6e45e7af02bd7428d3a82444fdc6af4abd9d
hyvuz <haneennasser1425@outlook.com>
2025-03-26 15:57:59 +0000

    Fixed 2 errors

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a single character to stdout
- * @c: The character to print
- * Return: 1 on success, -1 on failure
- */
+* _putchar - Writes a single character to stdout
+* @c: The character to print
+*
+* Return: 1 on success, -1 on failure
+*/
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+return (write(1, &c, 1));
 }
